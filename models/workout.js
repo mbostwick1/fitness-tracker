74696c9d61d74c855afe43e8413d54f4304129f4
Melanie Bostwick <mbostwick1@gmail.com>
2021-01-09 18:36:31 -0500

    server.js updated and created models, index and workout

---
+++ models/workout.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// creates  schema
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
+
+    //type of exercise to be logged
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type",
+        },
+
+        // exercise info to be logged
+        name: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        duration: {
+          type: Number,
+          required: true,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
